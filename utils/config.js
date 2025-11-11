dba5714dd55de23f04f88cf726838300c4b0f134
rirejuste047-rgb <rirejuste047@gmail.com>
2025-11-11 07:40:07 +0100

    Create config.js

---
+++ utils/config.js
@@ -0,0 +1,3 @@
+// utils/config.js
+export const OWNER_JID = "2250712668494@s.whatsapp.net"; // <-- remplace par le JID du créateur
+export const DATA_DIR = process.env.DATA_DIR || "./data"; // dossier où on stocke les JSON (optionnel)
